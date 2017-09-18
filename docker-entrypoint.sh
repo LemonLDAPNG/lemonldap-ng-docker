a3a5767cd4d36aa4c0f598a1ea270313050289fa
Clément OUDOT <clem.oudot@gmail.com>
2017-09-18 16:08:39 +0200

    Manage .js and .json configuration files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ fi
 echo "ServerName $SSODOMAIN" >> /etc/apache2/apache2.conf
 
 sed -i "s/example\.com/${SSODOMAIN}/g" /etc/lemonldap-ng/* \
-    /var/lib/lemonldap-ng/conf/lmConf-1.js /var/lib/lemonldap-ng/test/index.pl
+    /var/lib/lemonldap-ng/conf/lmConf-1.js* /var/lib/lemonldap-ng/test/index.pl
 
 find /etc/apache2/sites-available/ -name '*.conf' ! -name '000-default.conf' \
      -exec ln -sf {} /etc/apache2/sites-enabled/ \;
