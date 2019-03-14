7a5e1dd65241f13b2049bc1a5540019b75cf01c5
Neraud <neraud.login@gmail.com>
2019-03-14 15:40:16 +0100

    Add '*_HOSTNAME' environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 service cron start
 service anacron start
+
+if [ ! -z ${PORTAL_HOSTNAME+x} ]; then
+    sed -i -e "s/auth.example.com/${PORTAL_HOSTNAME}/g" /etc/lemonldap-ng/* /var/lib/lemonldap-ng/conf/lmConf-1.json
+fi
+if [ ! -z ${MANAGER_HOSTNAME+x} ]; then
+    sed -i -e "s/manager.example.com/${MANAGER_HOSTNAME}/g" /etc/lemonldap-ng/* /var/lib/lemonldap-ng/conf/lmConf-1.json
+fi
+if [ ! -z ${HANDLER_HOSTNAME+x} ]; then
+    sed -i -e "s/reload.example.com/${HANDLER_HOSTNAME}/g" /etc/lemonldap-ng/* /var/lib/lemonldap-ng/conf/lmConf-1.json
+fi
+if [ ! -z ${TEST1_HOSTNAME+x} ]; then
+    sed -i -e "s/test1.example.com/${TEST1_HOSTNAME}/g" /etc/lemonldap-ng/* /var/lib/lemonldap-ng/conf/lmConf-1.json
+fi
+if [ ! -z ${TEST2_HOSTNAME+x} ]; then
+    sed -i -e "s/test2.example.com/${TEST2_HOSTNAME}/g" /etc/lemonldap-ng/* /var/lib/lemonldap-ng/conf/lmConf-1.json
+fi
+
 sed -i "s/example\.com/${SSODOMAIN}/" /etc/lemonldap-ng/* /var/lib/lemonldap-ng/conf/lmConf-1.json
 
 sed -i -e "s/^logLevel.*/logLevel=${LOGLEVEL}\nlogger     = Lemonldap::NG::Common::Logger::Std/" /etc/lemonldap-ng/lemonldap-ng.ini
@@ -22,8 +39,8 @@ fi
 export SOCKET LISTEN PID USER GROUP
 
 if [ ! -z ${SOCKET+x} ]; then
-mkdir "$(dirname $SOCKET)"
-chown www-data "$(dirname $SOCKET)"
+    mkdir "$(dirname $SOCKET)"
+    chown www-data "$(dirname $SOCKET)"
 fi
 
 # Run the fastcgi server withing this session so that we can get logs in 
