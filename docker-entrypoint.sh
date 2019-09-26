2d9a69c81e1e80aeb663ad26fd33c80e4a406a3b
Paul Curie <paucur@worteks.com>
2019-09-26 11:54:06 +0200

    fix previous commit tabs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -40,7 +40,7 @@ sed -i -e 's/^;checkTime.*/checkTime = 1/' /etc/lemonldap-ng/lemonldap-ng.ini
 if [ ! -z ${FASTCGI_LISTEN_PORT+x} ]; then
     # Remove the SOCKET variable
     sed -i -e "s|^SOCKET=/run/llng-fastcgi-server/llng-fastcgi.sock|#SOCKET=/run/llng-fastcgi-server/llng-fastcgi.sock|" /etc/default/lemonldap-ng-fastcgi-server
-    
+
     # Add LISTEN variable
     echo "# Listen" >> /etc/default/lemonldap-ng-fastcgi-server
     echo "LISTEN=0.0.0.0:$FASTCGI_LISTEN_PORT" >> /etc/default/lemonldap-ng-fastcgi-server
@@ -48,7 +48,7 @@ if [ ! -z ${FASTCGI_LISTEN_PORT+x} ]; then
     # Update NGinx configuration from UNIX socket to TCP socket
     sed -i -e "s|fastcgi_pass unix:/var/run/llng-fastcgi-server/llng-fastcgi.sock|fastcgi_pass 0.0.0.0:$FASTCGI_LISTEN_PORT|" /etc/lemonldap-ng/*-nginx.conf
 
-	# Update upstream llng fastcgi to tcpsocket
+    # Update upstream llng fastcgi to tcpsocket
     sed -i -e "s|unix:/var/run/llng-fastcgi-server/llng-fastcgi.sock|0.0.0.0:$FASTCGI_LISTEN_PORT|" /etc/lemonldap-ng/portal-nginx.conf
 fi
 
